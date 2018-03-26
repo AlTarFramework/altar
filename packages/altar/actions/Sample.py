a3ac371026e2668f9ee534b2360beac91d63ed51
Michael Aivazis <michael.aivazis@para-sim.com>
2018-03-25 21:16:37 -0700

    altar.actions: primed {sample}, the action that samples the posterior distribution of a model

---
+++ packages/altar/actions/Sample.py
@@ -0,0 +1,39 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+#
+# (c) 2010-2018 california institute of technology
+# (c) 2013-2018 parasim inc
+# all rights reserved
+#
+
+# get the package
+import altar
+
+
+# declaration
+class Sample(altar.panel(), family='altar.actions.sample'):
+    """
+    Sample the posterior distribution of a model
+    """
+
+
+    # user configurable state
+    model = altar.properties.str()
+    model.tip = "the name of a model to sample"
+
+
+    # commands
+    @altar.export(tip="sample the posterior distribution of a model")
+    def default(self, plexus, **kwds):
+        """
+        Print the name of the app for configuration purposes
+        """
+        # show me
+        plexus.info.log(f"sampling '{self.model}'")
+        # all done
+        return
+
+
+# end of file
