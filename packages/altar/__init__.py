3041a7ebd61ba435e2fada5fc6d458577fc2ca86
Michael Aivazis <michael.aivazis@para-sim.com>
2018-03-26 23:12:13 -0700

    altar: package metadata adjustments

--- packages/altar/__init__.py
+++ packages/altar/__init__.py
@@ -3,8 +3,8 @@
 #
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 #
-# (c) 2010-2018 california institute of technology
 # (c) 2013-2018 parasim inc
+# (c) 2010-2018 california institute of technology
 # all rights reserved
 #
 
