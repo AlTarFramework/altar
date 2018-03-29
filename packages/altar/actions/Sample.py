7e3a62587d0bb9af68a51da6b2efac76ebad4264
Michael Aivazis <michael.aivazis@para-sim.com>
2018-03-28 23:14:45 -0700

    altar.actions: moved the resource discovery logic back to {sample}, for now; i think i need a resource map object to hold what the user asked and what is available

--- packages/altar/actions/Sample.py
+++ packages/altar/actions/Sample.py
@@ -30,6 +30,40 @@ class Sample(altar.panel(), family='altar.actions.sample'):
         """
         Print the name of the app for configuration purposes
         """
+        # make a channel
+        channel = plexus.info
+
+        # grab the programming model from the shell
+        mode = plexus.shell.model
+        # and the requested number of gpus per tasks
+        gpus = plexus.gpus
+
+        # show me
+        channel.line()
+        channel.line(f"mode: {mode}")
+
+        if mode == "mpi":
+            import mpi
+            channel.line(f"requested tasks: {plexus.shell.tasks}")
+            channel.line(f"actual tasks: {mpi.world.size}")
+
+        channel.line(f"requested gpus per task: {gpus}")
+        # attempt to
+        try:
+            # get support for cuda
+            import cuda
+        # if this fails
+        except ImportError:
+            # let me know
+            channel.line(f"available gpus on this node: 0")
+        # if it succeeds
+        else:
+            # show me
+            channel.line(f"available gpus on this node: {cuda.manager.count}")
+
+        # flush
+        channel.log()
+
         # all done
         return
 
