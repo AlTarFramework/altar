c094e75fdd8b6abb26a7b233c5c20d278553b5f7
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-02 16:04:55 +0300

    altar.simulations: primed {monitors} and {archivers}

---
+++ packages/altar/simulations/__init__.py
@@ -0,0 +1,14 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+#
+# (c) 2013-2018 parasim inc
+# (c) 2010-2018 california institute of technology
+# all rights reserved
+#
+
+# get the package
+import altar
+
+# end of file
