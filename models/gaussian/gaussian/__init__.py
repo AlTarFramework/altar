1092c12005a31ed65373879c556691b2ce63c328
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-05 10:39:33 +0300

    models.gaussian: primed the first real model

---
+++ models/gaussian/gaussian/__init__.py
@@ -0,0 +1,26 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+#
+# (c) 2013-2018 parasim inc
+# (c) 2010-2018 california institute of technology
+# all rights reserved
+#
+
+# the package
+import altar
+
+
+# implementations
+@altar.foundry(implements=altar.models.model, tip="a multi-parameter Gaussian model")
+def gaussian():
+    # grab the factory
+    from .Gaussian import Gaussian as gaussian
+    # attach its docstring
+    __doc__ = gaussian.__doc__
+    # and return it
+    return gaussian
+
+
+# end of file
