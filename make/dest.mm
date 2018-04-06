954130cc0949d525dbb691360eb9fbaf1306eff0
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-06 14:10:55 +0300

    make: {dest} now builds all the destination directories

--- make/dest.mm
+++ make/dest.mm
@@ -1,13 +1,14 @@
 # -*- Makefile -*-
 #
-# michael a.g. aïvázis
-# parasim
-# (c) 1998-2018 all rights reserved
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+#
+# (c) 2013-2018 parasim inc
+# (c) 2010-2018 california institute of technology
+# all rights reserved
 #
 
-$(prefix):
-	${call log.action,mkdir,$(prefix)}
-	$(mkdirp) $(prefix)
-
+$(dest) $(dest.py) $(dest.lib) $(dest.bin) $(dest.staging) :
+	${call log.action,mkdir,$@}
+	$(mkdirp) $@
 
 # end of file
