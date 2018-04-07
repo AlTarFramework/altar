c317428038a09ceac2ba39e696acdb0ad1b2e2c8
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-07 05:43:17 +0300

    make: added {packages}

---
+++ make/packages/fftw/config.mm
@@ -0,0 +1,28 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# add me to the pile
+packages += fftw
+
+# compiler flags
+fftw.flags ?=
+# enable {fftw} aware code
+fftw.defines := WITH_FFTW
+# the canonical form of the include directory
+fftw.incpath ?= $(fftw.dir)/include
+
+# linker flags
+fftw.ldflags ?=
+# the canonical form of the lib directory
+fftw.libpath ?= $(fftw.dir)/lib
+# the name of the library is flavor dependent
+fftw.libraries := fftw$(fftw.flavor)
+
+# my dependencies
+fftw.dependencies =
+
+# end of file
---
+++ make/packages/python/darwin-x86_64.mm
@@ -0,0 +1,14 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# on darwin, there are no additional libraries dependencies
+python.libraries +=
+
+# and no extra dependencies
+python.dependencies =
+
+# end of file
