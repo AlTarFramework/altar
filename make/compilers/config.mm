34c71b73edf2efeb802aad73d654488b8315ae19
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-07 05:43:38 +0300

    make: added compilers

---
+++ make/compilers/config.mm
@@ -0,0 +1,18 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# assemble the list of compilers
+#    order: defaults from the platform, then user configuration files, then mm command line
+compilers := \
+    $(platform.compilers) $(target.compilers) \
+    $($(developer).compilers) \
+    $(mm.compilers)
+
+# include the compiler specific configuration files
+include ${foreach compiler,$(compilers),compilers/$(compiler).mm}
+
+# end of file
---
+++ make/compilers/python.mm
@@ -0,0 +1,11 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# python3, by default; override by specifying python/python2 as the compiler
+include compilers/python/python3.mm
+
+# end of file
