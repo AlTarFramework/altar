90541354b32a435ed17aca28684f6a6ec4123b52
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-07 05:43:17 +0300

    make: added {packages}

---
+++ make/packages/python/linux-ppc64le.mm
@@ -0,0 +1,14 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# on linux, there are additional dependencies
+python.libraries += thread dl util
+
+# no extra dependencies
+python.dependencies =
+
+# end of file
