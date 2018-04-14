6b361cbda88730419e268136a73fe940f511f8a2
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-14 13:57:00 +0300

    models/linear: started fleshing out the {linear} model

---
+++ models/linear/linear/__init__.py
@@ -0,0 +1,26 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+#
+# (c) 2013-2018 parasim inc
+# (c) 2010-2018 california institute of technology
+# all rights reserved
+#
+
+# the package
+import altar
+
+
+# implementations
+@altar.foundry(implements=altar.models.model, tip="a linear model")
+def linear():
+    # grab the factory
+    from .Linear import Linear as linear
+    # attach its docstring
+    __doc__ = linear.__doc__
+    # and return it
+    return linear
+
+
+# end of file
