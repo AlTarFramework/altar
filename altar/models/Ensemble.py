347b00efbb753785aae219175d0b233102581e39
Michael Aivazis <michael.aivazis@para-sim.com>
2018-04-20 10:09:21 -0700

    altar.models: implemented {ensemble}, a model that consists of other altar models, and orchestrates the simulation updates by delegating to its members

--- altar/models/Ensemble.py
+++ altar/models/Ensemble.py
@@ -24,4 +24,78 @@ class Ensemble(Bayesian, family="altar.models.ensemble"):
     models.doc = "the collection of models in this ensemble"
 
 
+    # protocol obligations
+    @altar.export
+    def initialize(self, application):
+        """
+        Initialize the state of the model given an {application} context
+        """
+        # chain up
+        super().initialize(application=application)
+
+        # go through my models
+        for model in self.models:
+            # and initialize each one
+            model.initialize(application=application)
+
+        # all done
+        return self
+
+
+    # services
+    @altar.export
+    def initializeSample(self, step):
+        """
+        Fill {step.theta} with an initial random sample from my prior distribution.
+        """
+        # ask each of my models
+        for model in self.models:
+            # to initialize their portion of the samples in {step}
+            model.initializeSample(step=step)
+        # all done
+        return self
+
+
+    @altar.export
+    def priorLikelihood(self, step):
+        """
+        Fill {step.prior} with the likelihoods of the samples in {step.theta} in the prior
+        distribution
+        """
+        # ask each of my models
+        for model in self.models:
+            # to contribute to the computation of the prior likelihood
+            model.priorLikelihood(step=step)
+        # all done
+        return self
+
+
+    @altar.export
+    def dataLikelihood(self, step):
+        """
+        Fill {step.data} with the likelihoods of the samples in {step.theta} given the available
+        data. This is what is usually referred to as the "forward model"
+        """
+        # ask each of my models
+        for model in self.models:
+            # to contribute to the computation of the data likelihood
+            model.priorLikelihood(step=step)
+        # all done
+        return self
+
+
+    @altar.export
+    def verify(self, step, mask):
+        """
+        Check whether the samples in {step.theta} are consistent with the model requirements and
+        update the {mask}, a vector with zeroes for valid samples and non-zero for invalid ones
+        """
+        # ask each of my models
+        for model in self.models:
+            # to initialize their portion of the samples in {step}
+            model.verify(step=step, mask=mask)
+        # all done
+        return mask
+
+
 # end of file
