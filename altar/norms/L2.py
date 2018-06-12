f0162f53f8af438c5217fc819a22c66a27dc7566
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-06-13 01:33:38 +0300

    altar.norms: added the protocol; first pass at implementing the L2 norm

--- altar/norms/L2.py
+++ altar/norms/L2.py
@@ -15,10 +15,35 @@ from .Norm import Norm
 
 
 # declaration
-class L2(altar.component, familt="altar.norms.l2", implements=norm):
+class L2(altar.component, familt="altar.norms.l2", implements=Norm):
     """
     The L2 norm
     """
+
+
+    # interface
+    @altar.export
+    def eval(self, vectors, c_inv=None):
+        """
+        Compute the L2 norm of the given vectors
+        """
+        # allocate a vector for the result
+        norm = altar.vector(shape=vectors.columns)
+        # each column in {vectors} corresponds to a vector whose norm we are to compute
+        for column in range(vectors.columns):
+            # extract the vector
+            v = vectors.getColumn(column)
+            # and make a copy
+            vT = v.clone()
+            # pre-multiply by Cd^{-1}: use the upper triangle, no transpose, non-unit diagonal
+            v = altar.blas.dtrmv(
+                c_inv.upperTriangular, c_inv.opNoTrans, c_inv.nonUnitDiagonal,
+                c_inv, v)
+            # compute the dot product
+            norm[column] = altar.blas.ddot(vT, v)
+        # all done
+        return norm
+
 
 
 # end of file
