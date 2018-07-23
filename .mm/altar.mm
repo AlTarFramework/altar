f80fbeca2f941263a1193793075ddd2532ff3e5f
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-07-23 09:32:02 +0300

    mm: added support for building with {mm-4.0}

---
+++ .mm/altar.mm
@@ -0,0 +1,45 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# project meta-data
+altar.major := 1
+altar.minor := 0
+
+# altar consists of a python package
+altar.packages := altar.pkg
+# libraries
+altar.libraries := altar.lib
+# python extensions
+altar.extensions := altar.ext
+# and some tests
+altar.tests := altar.pkg.tests
+
+# the altar package meta-data
+altar.pkg.stem := altar
+altar.pkg.root := altar/
+altar.pkg.drivers := altar
+
+# libaltar meta-data
+altar.lib.stem := altar
+altar.lib.extern := gsl pyre
+altar.lib.c++.flags += $($(compiler.c++).std.c++17)
+
+# the altar extension meta-data
+altar.ext.stem := altar
+altar.ext.root := ext/
+altar.ext.pkg := altar.pkg
+altar.ext.wraps := altar.lib
+altar.ext.extern := altar.lib gsl pyre python
+# compile options for the sources
+altar.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
+
+# the altar test suite
+altar.pkg.tests.stem := altar
+altar.pkg.tests.prerequisites := altar.pkg altar.ext
+
+
+# end of file
---
+++ .mm/linear.mm
@@ -0,0 +1,22 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# project meta-data
+linear.major := 1
+linear.minor := 0
+
+# linear consists of a python package
+linear.packages := linear.pkg
+
+# the linear package meta-data
+linear.pkg.stem := linear
+linear.pkg.root := models/linear/linear/
+linear.pkg.bin := models/linear/bin/
+linear.pkg.pycdir := $(builder.dest.pyc)altar/models/linear/
+linear.pkg.drivers := linear
+
+# end of file
