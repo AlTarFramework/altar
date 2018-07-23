a08c1fe4a01641b3b9dfdddd67085e32e23bec19
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-07-23 09:32:02 +0300

    mm: added support for building with {mm-4.0}

---
+++ .mm/gaussian.mm
@@ -0,0 +1,22 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# project meta-data
+gaussian.major := 1
+gaussian.minor := 0
+
+# gaussian consists of a python package
+gaussian.packages := gaussian.pkg
+
+# the gaussian package meta-data
+gaussian.pkg.stem := gaussian
+gaussian.pkg.root := models/gaussian/gaussian/
+gaussian.pkg.bin := models/gaussian/bin/
+gaussian.pkg.pycdir := $(builder.dest.pyc)altar/models/gaussian/
+gaussian.pkg.drivers := gaussian
+
+# end of file
---
+++ .mm/mogi.mm
@@ -0,0 +1,22 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# project meta-data
+mogi.major := 1
+mogi.minor := 0
+
+# mogi consists of a python package
+mogi.packages := mogi.pkg
+
+# the mogi package meta-data
+mogi.pkg.stem := mogi
+mogi.pkg.root := models/mogi/mogi/
+mogi.pkg.bin := models/mogi/bin/
+mogi.pkg.pycdir := $(builder.dest.pyc)altar/models/mogi/
+mogi.pkg.drivers := mogi
+
+# end of file
