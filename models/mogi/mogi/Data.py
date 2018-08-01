59f62a07086f55b45800a27c786edb66e7322c62
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-08-01 12:18:17 +0300

    models/mogi: the {mogi} data layout object can now read/write itself into CSV files; added embedded documentation to all fields

--- models/mogi/mogi/Data.py
+++ models/mogi/mogi/Data.py
@@ -21,11 +21,48 @@ class Data(altar.tabular.sheet):
 
     # the layout
     oid = altar.tabular.int()
-    d = altar.tabular.float()
+    oid.doc = "an integer identifying the data source"
+
     x = altar.tabular.float()
+    x.doc = "the EW coordinate of the location of the source"
+
     y = altar.tabular.float()
+    y.doc = "the NS coordinate of the location of the source"
+
+    d = altar.tabular.float()
+    d.doc = "the depth of the source"
+
     theta = altar.tabular.float()
+    theta.doc = "the azimuthal angle of the LOS vector to the observing craft"
+
     phi = altar.tabular.float()
+    phi.doc = "the polar angle of the LOS vector to the observing craft"
+
+
+    # load data from a csv file
+    def read(self, uri):
+        """
+        Load a data set from a CSV file
+        """
+        # make a CSV writer
+        csv = altar.records.csv()
+        # pull data from the file and populate me with immutable records
+        self.pyre_immutable(data = csv.read(layout=self, uri=uri))
+        # all done
+        return self
+
+
+    # dump my data into a CSV file
+    def write(self, uri):
+        """
+        Save my data into a CSV file
+        """
+        # make a CSV writer
+        csv = altar.records.csv()
+        # ask it to save the data
+        csv.write(sheet=self, uri=uri)
+        # all done
+        return self
 
 
 # end of file
