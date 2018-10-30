aa4d7100a401baa41ca760b5c588b6a71110ceb2
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-10-30 11:02:51 -0700

    models/emhp: started implementing a diagnostic tool

---
+++ models/emhp/emhp/__init__.py
@@ -0,0 +1,26 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+#
+# (c) 2013-2018 parasim inc
+# (c) 2010-2018 california institute of technology
+# all rights reserved
+#
+
+# the package
+import altar
+
+
+# implementations
+@altar.foundry(implements=altar.models.model, tip="a diagnostic tool")
+def emhp():
+    # grab the factory
+    from .EMHP import EMHP as emhp
+    # attach its docstring
+    __doc__ = emhp.__doc__
+    # and return it
+    return emhp
+
+
+# end of file
