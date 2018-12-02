b5ae950a073425c5e947bcf886b35a1b353e05c2
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-12-02 08:13:31 -0800

    models/mogi: created bindings for the CUDA implementation

---
+++ models/mogi/ext/cudamogi/capsules.h
@@ -0,0 +1,26 @@
+// -*- C++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+//
+// (c) 2013-2018 parasim inc
+// (c) 2010-2018 california institute of technology
+// all rights reserved
+//
+
+#if !defined(altar_extensions_models_cudamogi_capsules_h)
+#define altar_extensions_models_cudamogi_capsules_h
+
+// capsules
+namespace altar {
+    namespace extensions {
+        namespace models {
+            namespace cudamogi {
+                const char * const source_capst = "altar.models.cudamogi.source";
+            }
+        }
+    }
+}
+
+#endif
+
+// end of file
---
+++ models/mogi/ext/cudamogi/cudamogi.cc
@@ -0,0 +1,90 @@
+// -*- C++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+//
+// (c) 2013-2018 parasim inc
+// all rights reserved
+//
+
+// for the build system
+#include <portinfo>
+// external dependencies
+#include <string>
+#include <Python.h>
+
+// the module method declarations
+#include "exceptions.h"
+#include "metadata.h"
+#include "source.h"
+
+
+// put everything in my private namespace
+namespace altar {
+    namespace extensions {
+        namespace models {
+            namespace cudamogi {
+                // the module method table
+                extern PyMethodDef module_methods[];
+                extern PyModuleDef module_definition;
+            } // of namespace cudamogi
+        } // of namespace models
+    } // of namespace extensions
+} // of namespace altar
+
+PyMethodDef
+altar::extensions::models::cudamogi::
+module_methods[] = {
+    // module metadata
+    // the version
+    { version__name__, version, METH_VARARGS, version__doc__ },
+
+    // source methods
+    // constructor
+    { newSource__name__, newSource, METH_VARARGS, newSource__doc__ },
+    // user supplied information
+    { data__name__, data, METH_VARARGS, data__doc__ },
+    { locations__name__, locations, METH_VARARGS, locations__doc__ },
+    { los__name__, los, METH_VARARGS, los__doc__ },
+    { oid__name__, oid, METH_VARARGS, oid__doc__ },
+    { layout__name__, layout, METH_VARARGS, layout__doc__ },
+    // and the residuals
+    { residuals__name__, residuals, METH_VARARGS, residuals__doc__ },
+
+    // sentinel
+    {0, 0, 0, 0}
+};
+
+// the module definition structure
+PyModuleDef
+altar::extensions::models::cudamogi::
+module_definition = {
+    // header
+    PyModuleDef_HEAD_INIT,
+    // the name of the module
+    "cudamogi",
+    // the module documentation string
+    "the mogi extension module with support for CUDA",
+    // size of the per-interpreter state of the module; -1 if this state is global
+    -1,
+    // the methods defined in this module
+    module_methods
+};
+
+// initialization function for the module
+// *must* be called PyInit_altar
+PyMODINIT_FUNC
+PyInit_cudamogi()
+{
+    // create the module
+    PyObject * module = PyModule_Create(&altar::extensions::models::cudamogi::module_definition);
+    // check whether module creation succeeded
+    if (!module) {
+        // and raise an exception if not
+        return 0;
+    }
+    // otherwise, we have an initialized module
+    // return the newly created module
+    return module;
+}
+
+// end of file
