ff7159357a8eab51cc9c51f43b112afc9785f71e
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-12-02 08:13:31 -0800

    models/mogi: created bindings for the CUDA implementation

---
+++ models/mogi/ext/cudamogi/metadata.cc
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+//
+// (c) 2013-2018 parasim inc
+// all rights reserved
+//
+
+#include <portinfo>
+#include <Python.h>
+
+#include "metadata.h"
+#include <altar/models/cudamogi/version.h>
+
+// version
+const char * const
+altar::extensions::models::cudamogi::version__name__ = "version";
+
+const char * const
+altar::extensions::models::cudamogi::version__doc__ = "the module version string";
+
+PyObject *
+altar::extensions::models::cudamogi::
+version(PyObject *, PyObject *)
+{
+    return Py_BuildValue("s", altar::models::cudamogi::version());
+}
+
+
+// end of file
