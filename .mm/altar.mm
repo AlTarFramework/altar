67c629287e5e7aa22300e438a39ab3315f46bf10
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-12-17 08:59:18 -0800

    cm: the only auto-recognized project is now {altar}; its {mm} file takes ownership of the build order of the models by including them explictly

--- .mm/altar.mm
+++ .mm/altar.mm
@@ -44,4 +44,7 @@ altar.pkg.tests.prerequisites := altar.pkg altar.ext
 tests.altar.application_run.clean = \
     ${addprefix $(altar.pkg.tests.prefix),llk.txt sigma.txt theta.txt}
 
+# models
+include emhp.def gaussian.def mogi.def linear.def
+
 # end of file
