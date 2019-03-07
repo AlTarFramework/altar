79067cad8cf4110e2939e307919ec0c255c9ada5
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-03-07 10:56:52 -0800

    happy new year

--- models/cdm/examples/synthetic/cdm.py
+++ models/cdm/examples/synthetic/cdm.py
@@ -4,8 +4,8 @@
 #
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 #
-# (c) 2013-2018 parasim inc
-# (c) 2010-2018 california institute of technology
+# (c) 2013-2019 parasim inc
+# (c) 2010-2019 california institute of technology
 # all rights reserved
 #
 
