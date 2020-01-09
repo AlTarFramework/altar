1e51867ef2ee0bdf4becae68cc2128092a07948d
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-01-09 20:02:21 +0200

    models/reverso: added the home of the extension module

---
+++ models/reverso/reverso/ext/__init__.py
@@ -0,0 +1,32 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+#
+# (c) 2013-2019 parasim inc
+# (c) 2010-2019 california institute of technology
+# all rights reserved
+#
+
+
+# attempt
+try:
+    # to load the extension module
+    from . import reverso as libreverso
+# if it fails
+except ImportError:
+    # the build/install must have failed somehow; we still have pure python support, so move on
+    pass
+
+
+# attempt
+try:
+    # to load the extension with the CUDA support
+    from . import cudareverso as libcudareverso
+# if it fails
+except ImportError:
+    # no worries
+    pass
+
+
+# end of file
