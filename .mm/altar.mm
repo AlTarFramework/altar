68b02cf3e50efd05c6960f7a7122a5ebf05f4798
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-01-29 15:50:07 +0200

    mm: adapted the build files to the new source layout; refactored {altar} into its own project file; simplified the model meta-data now that {mm} has better support for non-standard layouts

--- .mm/altar.mm
+++ .mm/altar.mm
@@ -5,46 +5,8 @@
 # (c) 1998-2020 all rights reserved
 #
 
-# project meta-data
-altar.major := $(repo.major)
-altar.minor := $(repo.minor)
-altar.micro := $(repo.micro)
-altar.revision := $(repo.revision)
-
-# altar consists of a python package
-altar.packages := altar.pkg
-# libraries
-altar.libraries := altar.lib
-# python extensions
-altar.extensions := altar.ext
-# and some tests
-altar.tests := altar.pkg.tests
-
-# the altar package meta-data
-altar.pkg.stem := altar
-altar.pkg.root := altar/
-altar.pkg.drivers := altar
-
-# libaltar meta-data
-altar.lib.stem := altar
-altar.lib.extern := gsl pyre
-altar.lib.c++.flags += $($(compiler.c++).std.c++17)
-
-# the altar extension meta-data
-altar.ext.stem := altar
-altar.ext.root := ext/
-altar.ext.pkg := altar.pkg
-altar.ext.wraps := altar.lib
-altar.ext.extern := altar.lib gsl pyre python
-# compile options for the sources
-altar.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
-
-# the altar test suite
-altar.pkg.tests.stem := altar
-altar.pkg.tests.prerequisites := altar.pkg altar.ext
-# individual test cases
-tests.altar.application_run.clean = \
-    ${addprefix $(altar.pkg.tests.prefix),llk.txt sigma.txt theta.txt}
+# the framework
+include altar.def
 
 # models
 include emhp.def gaussian.def mogi.def cdm.def linear.def
